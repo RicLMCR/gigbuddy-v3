39ee2a1306cdadcd3e05aab2cb91ee1f3e664d55
Richard <isit_ric@hotmail.com>
2023-06-04 18:19:22 +0100

    setup frontend inc react tsx prettier and eslint

---
+++ ui/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<h1>Gig Buddy V3</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
