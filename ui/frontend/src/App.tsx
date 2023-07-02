054af7f179bd6c1018d8e304ea416e4aa7dc068d
wumpfroot <markuskojo@msn.com>
2023-07-02 17:35:36 +0100

    Branch opened

--- ui/frontend/src/App.tsx
+++ ui/frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<h1>Gig Buddy V3</h1>
+        <h1>Gig Buddy V3</h1>
       </header>
     </div>
   );
