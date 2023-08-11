21bb2c6a8a1f4d85025145e505a9438cf931acf6
Richard <isit_ric@hotmail.com>
2023-08-11 13:00:34 +0100

    invalidate caches

--- backend/src/main/java/com/gigbuddy/backend/services/UserServiceImpl.java
+++ backend/src/main/java/com/gigbuddy/backend/services/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.gigbuddy.backend.services;
 
 import com.gigbuddy.backend.exceptions.EtAuthException;
+import com.gigbuddy.backend.respositories.UserRespository;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
